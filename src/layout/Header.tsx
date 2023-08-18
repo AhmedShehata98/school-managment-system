20b5a555cd80ae47140c38c82c221f03097def52
ahmedshehata98 <userstore98@gmail.com>
2023-08-18 15:49:56 +0300

    added sidemenu and header styles and handled app UI layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,77 @@
+"use client";
+
+import {
+  Avatar,
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import { BiGridAlt } from "react-icons/bi";
 import React from "react";
+import { MdOutlineNotificationsNone } from "react-icons/md";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <HStack
+      as={"header"}
+      className="w-full py-3 px-6 shadow-sm bg-white border-b"
+    >
+      <FormControl>
+        <InputGroup>
+          <InputLeftElement>
+            <Search2Icon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            variant={"filled"}
+            type="search"
+            placeholder="search for somthing ..."
+            id="search"
+            name="search"
+            width={"20%"}
+          />
+        </InputGroup>
+      </FormControl>
+      <HStack width={"33%"} justifyContent={"end"}>
+        <Button
+          type="button"
+          variant={"ghost"}
+          w={"14"}
+          p={"0"}
+          color={"blue.700"}
+          colorScheme="blue"
+          rounded={"full"}
+        >
+          <BiGridAlt className="text-xl" />
+        </Button>
+        <Button
+          type="button"
+          variant={"ghost"}
+          w={"14"}
+          color={"blue.700"}
+          colorScheme="blue"
+          rounded={"full"}
+        >
+          <MdOutlineNotificationsNone className="text-xl" />
+        </Button>
+        <Wrap>
+          <WrapItem>
+            <Avatar
+              name="ahmed shehata"
+              src="https://bit.ly/sage-adebayo"
+              size={"md"}
+            />
+          </WrapItem>
+        </Wrap>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default Header;
--- src/layout/SideMenu.tsx
+++ src/layout/SideMenu.tsx
@@ -4,7 +4,11 @@ import Logo from "@/components/Logo";
 import { MdQuiz } from "react-icons/md";
 
 const SideMenu = () => {
-  return <aside>adasdasssssssssssssssss</aside>;
+  return (
+    <aside className="max-md:w-full w-1/5 h-screen bg-white border-r shadow-sm">
+      adasdasssssssssssssssss
+    </aside>
+  );
 };
 
 export default SideMenu;
